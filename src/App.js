9e27e3c45e9ef2841d810ab093a6f1b4fd1ece62
ahasannahid <ahasan15-9072@diu.edu.bd>
2022-11-09 11:06:00 +0600

    update header

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import { RouterProvider } from 'react-router-dom';
+import router from './Router/Route/Route';
 
 function App() {
   return (
-    <div className="App">
-      <button className="btn">Button</button>
-      <button className="btn btn-primary">Button</button>
-      <button className="btn btn-secondary">Button</button>
-      <button className="btn btn-accent">Button</button>
-      <button className="btn btn-ghost">Button</button>
-      <button className="btn btn-link">Button</button>
-    </div>
+    <div  className='max-w-screen-xl mx-auto'>
+    <RouterProvider router={router}></RouterProvider>
+  </div>
   );
 }
 
---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import banner from '../../../src/assets/banner.jpg'
+
+const Home = () => {
+    return (
+        <div>
+            <img src={banner} alt="" className='h-1/2' />
+        </div>
+    );
+};
+
+export default Home;
