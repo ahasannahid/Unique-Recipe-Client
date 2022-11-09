385f18cfbf8370532c595b60ba0775214342908b
ahasannahid <ahasan15-9072@diu.edu.bd>
2022-11-09 19:37:31 +0600

    added google sign in

--- src/Router/Route/Route.js
+++ src/Router/Route/Route.js
@@ -2,6 +2,8 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../../layout/Main";
 import Allrecipe from "../../Pages/Allrecipe/Allrecipe";
 import Home from "../../Pages/Home/Home";
+import Login from "../../Pages/Login/Login";
+import SignUp from "../../Pages/Signup/Signup";
 import RecipeDetails from "../../Shared/RecipeDetails/RecipeDetails";
 
 const router = createBrowserRouter([
@@ -23,7 +25,15 @@ const router = createBrowserRouter([
                 path: '/details/:id',
                 element: <RecipeDetails></RecipeDetails>,
                 loader: ({ params }) => fetch(`http://localhost:5000/recipes/${params.id}`)
-            }
+            },
+            {
+                path: '/login',
+                element: <Login></Login>
+            },
+            {
+                path: '/signup',
+                element: <SignUp></SignUp>
+            },
         ]
     }
 ]);
--- src/Shared/Header/Header.js
+++ src/Shared/Header/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
         {
 
             <>
-                <li className='font-semibold'><Link to='/orders'>Order</Link></li>
+                <li className='font-semibold'><Link to='/login'>Login</Link></li>
             </>
 
         }
