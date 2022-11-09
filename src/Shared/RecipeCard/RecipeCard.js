4097f093ffee1a9d29a077742354f34a9f36e653
ahasannahid <ahasan15-9072@diu.edu.bd>
2022-11-09 21:04:07 +0600

    add react photo view

--- src/Shared/RecipeCard/RecipeCard.js
+++ src/Shared/RecipeCard/RecipeCard.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import 'react-photo-view/dist/react-photo-view.css';
+import { PhotoProvider, PhotoView } from 'react-photo-view';
 
 const RecipeCard = ({ recipe }) => {
     const { rating, total_view, cook, recipe_name, price, img_url, recipe_details, _id } = recipe;
     return (
         <div className="card card-compact w-96 bg-base-100 shadow-xl">
-            <figure><img src={img_url} alt="" /></figure>
+            <figure>
+                <PhotoProvider>
+                    <PhotoView src={img_url}>
+                        <img src={img_url} alt="" />
+                    </PhotoView>
+                </PhotoProvider>
+
+            </figure>
             <div className="card-body">
-            <div className="card-body">
+                <div className="card-body">
                     <h2 className="card-title">{recipe_name}</h2>
-                    <p>How to make: {recipe_details.slice(0,100)} <Link className='text-primary' to={`/details/${_id}`}>...See More</Link></p>
+                    <p>How to make: {recipe_details.slice(0, 100)} <Link className='text-primary' to={`/details/${_id}`}>...See More</Link></p>
                 </div>
-                <div className="card-actions justify-end">
+                <div className="card-actions justify-between">
+                    <button className="btn btn-info">Price: $ {price}</button>
                     <Link to={`/details/${_id}`}>
                         <button className="btn btn-primary">Details</button>
                     </Link>
