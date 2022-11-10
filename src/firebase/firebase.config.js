c29576393322d326b3449beef35f620b8329510c
ahasannahid <ahasan15-9072@diu.edu.bd>
2022-11-10 22:47:34 +0600

    set environment variable

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBN5YayInh7IYKQsR_kf2nxBc675F1dNe8",
-  authDomain: "unique-recipe.firebaseapp.com",
-  projectId: "unique-recipe",
-  storageBucket: "unique-recipe.appspot.com",
-  messagingSenderId: "747347131072",
-  appId: "1:747347131072:web:bae72599639e3affb0cb25"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId
 };
 
 // Initialize Firebase
